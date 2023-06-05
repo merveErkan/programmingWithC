d883c959fb7b9be69bb649d47ced64737f5e6643
Merve Erkan <117810757+merveErkan@users.noreply.github.com>
2023-06-05 14:58:41 +0300

    Create permutationWithRF.c

---
+++ permutationWithRF.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int F(int a){
+    if(a == 0)
+        return 1;
+    return a * F(a - 1);
+}
+int P(int x, int y){
+    if(x > y)
+    return F(x) / F(x - y);
+}
+int main(){
+    int x, y, result;
+    printf("enter x and y\n");
+    scanf("%d %d", &x, &y);
+    result = P(x, y);
+    printf("result: %d", result);
+    return 0; 
+}
